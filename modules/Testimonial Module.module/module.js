aa15d332f01855cacb1293fbfe77f04a85425e5e
sonu-develops <sonukumarkcl@gmail.com>
2023-06-15 16:54:08 +0530

    version 1

---
+++ modules/Testimonial Module.module/module.js
@@ -0,0 +1,19 @@
+$('#test-slider').owlCarousel({
+    loop:true,
+    margin:10,
+  autoplay:true,
+    autoplayTimeout:3000,
+    nav:false,
+  dot:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
